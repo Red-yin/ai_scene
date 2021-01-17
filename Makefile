95d896ef94be3c4ef074d5a99a3ddb778cffada8
yinzhiyuan <yinzhiyuan@orvibo.com>
2021-01-17 21:06:11 +0800

    aiscene

---
+++ Makefile
@@ -0,0 +1,33 @@
+
+TARGET = aiscene
+SOURCE_FILES = ${wildcard ./src/*.c}
+CC = gcc
+RM = rm -rf
+LDFLAG = -lpthread -lm -lmylog
+LDFLAG += -L./lib/lib
+LDFLAG += -L./extern/lib
+INCLUDE = -I./lib/include
+INCLUDE += -I./extern/lib/include
+MACRO = 
+
+OBJECTS = $(patsubst %.c,%.o, $(SOURCE_FILES))
+#OBJECTS = $(foreach item,$(SOURCE_FILES),$(CC) -c $(item) -o $(patsubst %.c,%.o, $(item)) $(INCLUDE) $(MACRO))
+#gcc -E  hello.c  -o hello.i
+#PRECOMPILE_FILES := $(foreach item,$(SOURCE_FILES),$(CC) -E $(item) -o $(patsubst %.c,%.i, $(item)) $(INCLUDE) $(MACRO))
+#COMPILE_FILES := $(foreach item,$(SOURCE_FILES),$(CC) -S $(item) -o $(patsubst %.c,%.s, $(item)) $(INCLUDE) $(MACRO))
+#COMPILE_FILES = $(patsubst %.c,%.s, $(SOURCE_FILES))
+
+$(TARGET): $(OBJECTS)
+	$(CC) -o $@ $^ $(LDFLAG)
+
+
+#gcc  -c  hello.s  -o  hello.o
+$(OBJECTS):$(SOURCE_FILES)
+	$(foreach item, $(SOURCE_FILES),$(CC) -c $(item) -o $(patsubst %.c,%.o, $(item)) $(INCLUDE);)
+
+#gcc  -S  hello.i   -o  hello.s
+#$(COMPILE_FILES):$(PRECOMPILE_FILES)
+	#$(CC) -S $^
+
+clean:
+	$(RM) $(TARGET) $(OBJECTS)
---
+++ src/main.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cJSON.h"
+#include "slog.h"
+
+static const char *filename = "./config/AI_scene.json";
+
+
+void aiscene_run(const char *scene);
+
+int main(void){
+	char buf[512] = {0};
+	while(1){
+		raw("input scene name: ");
+		scanf("%s", buf);
+		if(strcmp(buf, "q") == 0){
+			exit(0);
+		}
+		aiscene_run(buf);
+	}
+	return 0;
+}
+
+cJSON *file_read(const char *filename)
+{
+	FILE *fp = fopen(filename, "r");
+	if(fp == NULL){
+		err("%s is not exist", filename);
+		return NULL;
+	}
+	fseek(fp, 0, SEEK_END);
+	long len = ftell(fp);
+	if(len <= 0){
+		err("%s is empyt", filename);
+		goto error;
+	}
+	char *file_buffer = (char *)malloc(len + 1);
+	if(file_buffer == NULL){
+		err("system memory is less than %d", len);
+		goto error;
+	}
+	fseek(fp, 0, SEEK_SET);
+	fread(file_buffer, len, 1, fp);
+	cJSON *root = cJSON_Parse(file_buffer);
+	if(root == NULL){
+		err("%s is not json", file_buffer);
+		goto error;
+	}
+	fclose(fp);
+	return root;
+
+error:
+	if(fp){
+		fclose(fp);
+	}
+	return NULL;
+}
+
+void aiscene_run(const char *name)
+{
+	cJSON *content = file_read(filename);
+	if(content == NULL){
+		exit(-1);
+	}
+	cJSON *scene = cJSON_GetObjectItem(content, "scene");
+	if(scene == NULL){
+		err("%s has no scene", filename);
+		exit(-1);
+	}
+	int scene_array_len = cJSON_GetArraySize(scene);
+	dbg("scene number: %d", scene_array_len);
+	int i;
+	for(i = 0; i < scene_array_len; i++){
+		cJSON *scene_item = cJSON_GetArrayItem(scene, i);
+		if(scene_item == NULL){
+			err("%s scene is not array format", filename);
+			exit(-1);
+		}
+		cJSON *sceneName = cJSON_GetObjectItem(scene_item, "sceneName");
+		if(sceneName == NULL){
+			err("%d has no %s", i);
+			exit(-1);
+		}
+		if(sceneName->valuestring == name){
+			cJSON *variable = cJSON_GetObjectItem(scene_item, "variable");
+			if(variable == NULL){
+				err("scene %s has no variable", name);
+				return;
+			}
+			cJSON *selector = cJSON_GetObjectItem(variable, selector);
+			if(selector == NULL){
+				wrn("%s variable has no selector", name);
+				return;
+			}
+			aiscene_selector(selector);
+		}
+	}
+}
